48b924412630aff274f598b6f24a8cd18ed6ead0
JoshuaJewell <enquiries@joshuajewell.dev>
2025-02-23 22:38:03 +0000

    Created encode function.

--- src/textgen.jl
+++ src/textgen.jl
@@ -1,26 +1,15 @@
 include("decoder.jl")
-include("encoders.jl")
+include("encoder.jl")
 include("tensorsmerge.jl")
 
 using JLD2, Random
-using .Decoder, .Encoders, .TensorsMerge
+using .Decoder, .Encoder, .TensorsMerge
 
 Random.seed!(1234)
 
 finewebcontext = read("../data/contexts/fineweb-top5000.txt", String)
 finewebtensors = jldopen("../data/tensordicts/fineweb-top5000.tensordict", "r") do file
     file["data"]
-end
-
-# Encode a tensordict from context
-function encode(context, mode = "default", end_punctuation = [".", "!", "?"], exclude = [" ", "(", ")", "\"", "*"])
-    initT = time()
-    if mode == "sanger"
-        sanger_encoder(context, fragment_size = 1)
-    else
-        default_encoder(context, end_punctuation, exclude)
-    end
-    println("\nEncoded in $(time() - initT) s")
 end
 
 stream = false
